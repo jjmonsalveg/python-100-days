001eab8a61bf9daa6f773f69e26f71d56b1554dd
jdgg <giraldojose1996@gmail.com>
2024-07-02 06:26:24 -0500

    feature: Day 16 OOO coffee machine

---
+++ day-16/main.py
@@ -0,0 +1,46 @@
+from enum import Enum
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+class MachineOptions(Enum):
+    CAPPUCCINO = "cappuccino"
+    ESPRESSO = "espresso"
+    LATTE = "latte"
+    OFF = "off"
+    REPORT = "report"
+
+def coffee_machine() -> None:
+    on = True
+    coffee_maker = CoffeeMaker()
+    menu = Menu()
+    money_machine = MoneyMachine()
+
+    while on:
+        try:
+            choice = MachineOptions(input(f"What would you like? ({menu.get_items()})").casefold())
+
+            match choice:
+                case MachineOptions.LATTE |  MachineOptions.CAPPUCCINO | MachineOptions.ESPRESSO:
+                    menu_item = menu.find_drink(choice.value)
+                    enough_resources = coffee_maker.is_resource_sufficient(menu_item)
+
+                    if not enough_resources:
+                        continue
+
+                    enough_money = money_machine.make_payment(menu_item.cost)
+
+                    if not enough_money:
+                        continue
+                    
+                    coffee_maker.make_coffee(menu_item)
+
+                case MachineOptions.OFF:
+                    on = False
+                case MachineOptions.REPORT:
+                    coffee_maker.report() 
+        except ValueError as e:
+            print(e)
+
+
+coffee_machine()
