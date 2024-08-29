95c7a2fadb31c498a9f664dd4e5c273ed7bb38c8
Jonathan Monsalve <j.jmonsalveg@gmail.com>
2024-08-29 10:57:10 -0600

    chore: Run formatter

--- day-22/main.py
+++ day-22/main.py
@@ -7,8 +7,9 @@
 # TODO. Detect when paddle misses
 # TODO. Keep score
 from turtle import Screen
-from paddle import Paddle
+
 from ball import Ball
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -16,7 +17,7 @@ screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
 
-r_paddle = Paddle(( 350, 0 ))
+r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
 screen.listen()
