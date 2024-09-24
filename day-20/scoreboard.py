2ceef5964002f94ab200c565950c15598f5d6da0
Jonathan Javier Monsalve Giraldo <j.jmonsalveg@gmail.com>
2024-09-24 05:38:05 -0600

    Day 24 - Management of files (#11)
    * Day 20: add support of saving score in files
    
    * chore: running ruff
    
    * feature(day-24): Management of files
    
    ---------
    
    Co-authored-by: jdgg <giraldojose1996@gmail.com>

--- day-20/scoreboard.py
+++ day-20/scoreboard.py
@@ -8,6 +8,10 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
+        
         self.color("white")
         self.penup()
         self.hideturtle()
@@ -15,13 +19,18 @@ class ScoreBoard(Turtle):
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", True, align=ALIGNMENT, font=FONT)
+    def reset(self) -> None:
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.high_score))
+
+        self.score = 0
+        self.update_scoreboard() 
 
     def update_scoreboard(self):
+        self.clear()
         self.goto(0, 270)
-        self.write(f"Score: {self.score}", True, align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", True, align=ALIGNMENT, font=FONT)
--- day-20/snake.py
+++ day-20/snake.py
@@ -24,6 +24,13 @@ class Snake:
         segment.penup()
         segment.goto(position)
         self.segments.append(segment)
+    
+    def reset(self):
+        for segment in self.segments:
+            segment.hideturtle()
+            
+        self.segments.clear()
+        self._create_snake()
 
     def extend(self):
         self.add_segment(self.segments[-1].position())
