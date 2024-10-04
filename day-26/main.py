d048f009e1c05db0f09d21e1aef72428b1e84f61
jdgg <giraldojose1996@gmail.com>
2024-10-04 06:20:42 -0500

    feature(day-26): Solve nato problem

---
+++ day-26/main.py
@@ -0,0 +1,7 @@
+import pandas
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+nato_phonetics = { row.letter: row.code for (_, row) in data.iterrows()}
+
+user_input = input("Enter a word:").upper()
+nato_map = [nato_phonetics[letter] for letter in list(user_input)]
+print(nato_map)
