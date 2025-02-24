a908b12ec88e007f89c945ac11e7439a7475c63b
Jonathan Javier Monsalve Giraldo <j.jmonsalveg@gmail.com>
2025-02-24 05:57:55 -0600

    Day 45: Intermediate+ Web Scraping with Beautiful Soup (#34)
    * chore(day-45): WIP
    
    Co-authored-by: jdg16 <jdg16@users.noreply.github.com>
    
    * feat(day 45): Intermediate+ Web Scraping with Beautiful Soup
    
    Co-authored-by: jdg16 <jdg16@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: jdg16 <jdg16@users.noreply.github.com>

---
+++ day-45/bs4-start/playground.py
@@ -0,0 +1,29 @@
+from os import name
+from bs4 import BeautifulSoup
+
+with open("website.html", mode="r") as file:
+    content = file.read()
+
+soup = BeautifulSoup(content, "html.parser")
+
+all_member_tags = soup.find_all(name="a")
+
+for tag in all_member_tags:
+    print(tag.getText())
+    print(tag.get("href"))
+
+heading = soup.find(name="h1", id="name")
+print(heading)
+
+h3_heading = soup.find(name="h3", class_="heading")
+print(h3_heading.getText)
+
+company_url = soup.select_one(selector="p a")
+print(company_url)
+
+name = soup.select_one(selector="#name")
+print(name)
+
+headings = soup.select_one(selector=".heading")
+print(headings)
+
